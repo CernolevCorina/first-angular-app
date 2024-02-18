a4aeea997a5d377f5169bd1c619d516ce5c3622d
CernolevCorina <corina.cernolev@gmail.com>
2024-02-18 22:17:06 +0200

    add intro and facts sections

---
+++ src/app/components/modules/home/facts/facts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-facts',
+  standalone: true,
+  imports: [],
+  templateUrl: './facts.component.html',
+  styleUrl: './facts.component.scss'
+})
+export class FactsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import {IntroComponent} from "../../components/modules/home/intro/intro.component";
+import {FactsComponent} from "../../components/modules/home/facts/facts.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
+  imports: [
+    IntroComponent,
+    FactsComponent
+  ],
+  templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
